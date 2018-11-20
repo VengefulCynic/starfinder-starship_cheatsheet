b6f82d3a6f3ab53cc9f59a704d081dd97d22ea8f
Scholl <JoshuaScholl@gmail.com>
2018-11-20 14:01:24 -0600

    Making the javascript suck less and adding some artifacts.

--- jsc/cheatsheet.js
+++ jsc/cheatsheet.js
@@ -1,109 +1,138 @@
-var function captain_Demand(tier)
+function captain_Demand(tier)
 {
    // DC is 15+1.5*tier
    return (15 + Math.floor(1.5*tier) );
 }
 
-var function captain_Encourage(tier)
+function captain_Encourage(tier)
 {
    // DC of the Diplomacy check is 15; the DC for checks using other skills remains 10. 
    // DC is 15+1.5*tier
    return (15 + Math.floor(1.5*tier) );
 }
 
-var function captain_Taunt(enemyTier)
+function captain_Taunt(enemyTier)
 {
    // DC is 15+1.5*enemyTier
    return (15 + Math.floor(1.5*enemyTier) );
 }
 
-var function captain_Orders(tier)
+function captain_Orders(tier)
 {
    // DC is 15+1.5*tier
    return (15 + Math.floor(1.5*tier) );
 }
 
-var function captain_MovingSpeech(tier)
+function captain_MovingSpeech(tier)
 {
    // DC is 20+1.5*tier
    return (20 + Math.floor(1.5*tier) );
 }
 
-var function engineer_Divert(tier)
+function engineer_Divert(tier)
 {
    // DC is 10+1.5*(tier)
    return (10 + Math.floor(1.5*tier) );
 }
 
-var function engineer_HoldItTogether(tier)
+function engineer_HoldItTogether(tier)
 {
    // DC is 15+1.5*tier
    return (15 + Math.floor(1.5*tier) );
 }
 
-var function engineer_PatchGlitch(tier)
+function engineer_PatchGlitch(tier)
 {
    // DC is 10+1.5*(tier)
    return (10 + Math.floor(1.5*tier) );
 }
 
-var function engineer_PatchMalfunction(tier)
+function engineer_PatchMalfunction(tier)
 {
    // DC is 15+1.5*tier
    return (15 + Math.floor(1.5*tier) );
 }
 
-var function engineer_PatchWrecked(tier)
+function engineer_PatchWrecked(tier)
 {
    // DC is 20+1.5*tier
    return (20 + Math.floor(1.5*tier) );
 }
 
-var function engineer_Overpower(tier)
+function engineer_Overpower(tier)
 {
    // DC is 15+1.5*tier
    return (15 + Math.floor(1.5*tier) );
 }
 
-var function engineer_QuickFix(tier)
+function engineer_QuickFix(tier)
 {
    // DC is 20+1.5*tier
    return (20 + Math.floor(1.5*tier) );
 }
 
-var function pilot_BackOffStunt(tier)
+function pilot_BackOffStunt(tier)
 {
    // DC is 10+1.5*(tier)
    return (10 + Math.floor(1.5*tier) );
 }
 
-var function pilot_BarrelRollStunt(tier)
+function pilot_BarrelRollStunt(tier)
 {
    // DC is 10+1.5*(tier)
    return (10 + Math.floor(1.5*tier) );
 }
 
-var function pilot_EvadeStunt(tier)
+function pilot_EvadeStunt(tier)
 {
    // DC is 10+1.5*(tier)
    return (10 + Math.floor(1.5*tier) );
 }
 
-var function pilot_FlipAndBurnStunt(tier)
+function pilot_FlipAndBurnStunt(tier)
 {
    // DC is 15+1.5*tier
    return (15 + Math.floor(1.5*tier) );
 }
 
-var function pilot_FlyByStunt(tier)
+function pilot_FlyByStunt(tier)
 {
    // DC is 15+1.5*tier
    return (15 + Math.floor(1.5*tier) );
 }
 
-var function pilot_SlideStunt(tier)
+function pilot_SlideStunt(tier)
 {
    // DC is 10+1.5*(tier)
    return (10 + Math.floor(1.5*tier) );
 }
 
+function science_Balance(tier)
+{
+    // DC is 10+1.5*(tier)
+    return (10 + Math.floor(1.5*tier) );
+}
+
+function science_Scan(enemy_tier, countermeasure_bonus)
+{
+    // DC is 5+1.5*(enemy_tier)+countermeasure_bonus
+    return (5 + countermeasure_bonus + Math.floor(1.5*enemy_tier) );
+}
+
+function science_TargetSystem(enemy_tier, countermeasure_bonus)
+{
+    // DC is 5+1.5*(enemy_tier)+countermeasure_bonus
+    return (5 + countermeasure_bonus + Math.floor(1.5*enemy_tier) );
+}
+
+function science_LockOn(enemy_tier, countermeasure_bonus)
+{
+    // DC is 5+1.5*(enemy_tier)+countermeasure_bonus
+    return (5 + countermeasure_bonus + Math.floor(1.5*enemy_tier) );
+}
+
+function science_ImproveCountermeasures(enemy_tier, countermeasure_bonus)
+{
+    // DC is 5+1.5*(enemy_tier)+countermeasure_bonus
+    return (5 + countermeasure_bonus + Math.floor(1.5*enemy_tier) );
+}
